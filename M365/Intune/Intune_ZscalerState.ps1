e820d3fb8064ef5fd234523cf1bda9b81c2c2b2a
kassainov <kassainov@gmail.com>
2023-02-03 17:59:03 +0100

    Refactored Zscaler compliance check script

--- M365/Intune/Intune_ZscalerState.ps1
+++ M365/Intune/Intune_ZscalerState.ps1
@@ -4,34 +4,35 @@
     registry keys: https://help.zscaler.com/z-app/zscaler-app-registry-keys
 #>
 
+# Default values
 $ZscalerState = @{"State" = "Default value"; "ZNW" =  $false; "ZPA" =  $false; "ZWS" = $false}
-# Getting registry keys
+
+# Getting registry keys and comaping with desired state
 try {
     $RegZSApp = Get-ItemProperty -Path Registry::HKEY_CURRENT_USER\Software\Zscaler\App -ErrorAction Stop
     $ZNW = $RegZSApp.ZNW_State
     $ZPA = $RegZSApp.ZPA_State
     $ZWS = $RegZSApp.ZWS_State
+    
+    # Check States
     if ($ZNW -eq "" -or $ZPA -eq "" -or $ZWS -eq ""){
             $ZscalerState.State = "Registry key values are empty"
+    }
+    else {
+        $ZNWGoodStates = @("TRUSTED_VPN", "TRUSTED")
+        if ($ZNWGoodStates.Contains($ZNW)) {
+            $ZscalerState = @{"State" = "Connected"; "ZNW" = $true; "ZPA" = $true; "ZWS" = $true}
         }
+        else {
+            $ZscalerState.State = "Not connected"
+        }
+    }
 }
 catch [System.Management.Automation.ItemNotFoundException] {
     $ZscalerState.State = "Not enrolled"
-    return $ZscalerState | ConvertTo-Json -Compress
 }
 catch {
     $ZscalerState.State = "Error happened while reading registry value"
-    return $ZscalerState | ConvertTo-Json -Compress
 }
-
-# Check States
-$ZNWGoodStates = @("TRUSTED_VPN", "TRUSTED")
-if ($ZNWGoodStates.Contains($ZNW)) {
-    $ZscalerState = @{"State" = "Connected"; "ZNW" = $true; "ZPA" = $true; "ZWS" = $true}
-}
-else {
-    $ZscalerState.State = "Not connected"
-}
-
 # Returning state
 return $ZscalerState | ConvertTo-Json -Compress

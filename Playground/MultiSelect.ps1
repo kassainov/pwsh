04eac42549f21ec726ea56150ce069f71587113a
kassainov <kassainov@gmail.com>
2023-06-15 16:53:59 +0200

    Playgound: multiselect form to use as module

---
+++ Playground/MultiSelect.ps1
@@ -0,0 +1,75 @@
+<#
+    .DESCRIPTION
+    The module can recieve list and return selected value from GUI
+    
+    .PARAMETER List
+    The list of values to selet from
+
+    .EXAMPLE
+    list = @("Hello!", "Bye!", "Nice to meet you!")
+    MultiSelect(list) 
+    << will open form that will ask to select one value and hit "OK".
+    >> "Hello!"
+#>
+
+Param(
+    [Parameter(Mandatory=$true)]
+    [String]
+    $List
+)
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$form = New-Object System.Windows.Forms.Form
+$form.Text = 'OPTIONS'
+$form.Size = New-Object System.Drawing.Size(300,200)
+$form.StartPosition = 'CenterScreen'
+
+$OKButton = New-Object System.Windows.Forms.Button
+$OKButton.Location = New-Object System.Drawing.Point(75,120)
+$OKButton.Size = New-Object System.Drawing.Size(75,23)
+$OKButton.Text = 'OK'
+$OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $OKButton
+$form.Controls.Add($OKButton)
+
+$CancelButton = New-Object System.Windows.Forms.Button
+$CancelButton.Location = New-Object System.Drawing.Point(150,120)
+$CancelButton.Size = New-Object System.Drawing.Size(75,23)
+$CancelButton.Text = 'Cancel'
+$CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$form.CancelButton = $CancelButton
+$form.Controls.Add($CancelButton)
+
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(10,20)
+$label.Size = New-Object System.Drawing.Size(280,20)
+$label.Text = 'PLEASE SELECT ITEM:'
+$form.Controls.Add($label)
+
+$listBox = New-Object System.Windows.Forms.Listbox
+$listBox.Location = New-Object System.Drawing.Point(10,40)
+$listBox.Size = New-Object System.Drawing.Size(260,20)
+
+#$listBox.SelectionMode = 'MultiExtended'
+
+[void] $listBox.Items.Add('Item 1')
+[void] $listBox.Items.Add('Item 2')
+[void] $listBox.Items.Add('Item 3')
+[void] $listBox.Items.Add('Item 4')
+[void] $listBox.Items.Add('Item 5')
+
+$listBox.Height = 70
+$form.Controls.Add($listBox)
+$form.Topmost = $true
+
+$result = $form.ShowDialog()
+
+if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+{
+    $x = $listBox.SelectedItems
+    $x
+}
+else {
+    write-output "nothing selected"
+}
